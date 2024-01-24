3754337efe0af1b14792deb205867b550f105894
SavageHobbies <112442093+SavageHobbies@users.noreply.github.com>
2024-01-24 09:13:05 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-import * as firebaseui from 'firebaseui-ja';
+import * as firebaseui from './node_modules/firebaseui-ja';
 
 const ui = new firebaseui.auth.AuthUI(firebase.auth());
 // This callback function is triggered when the DOM is loaded.
