f5907b07ec8baa223685841f3a8870557514e4fb
SavageHobbies <112442093+SavageHobbies@users.noreply.github.com>
2024-01-24 08:32:42 -0500

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"; // Importing getAuth
 
